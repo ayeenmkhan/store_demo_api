4883dffe9c91f609df8fbdbc8108b1f3d5aea77e
ayeenmkhan <gbtechsolutionss@gmail.com>
2020-11-15 19:47:51 +0500

    store demo apis

--- routes/product/product.js
+++ routes/product/product.js
@@ -1,7 +1,6 @@
 const express= require('express');
 const router= express.Router();
 const { getProduct, addProduct, deleteProduct, updateProduct }= require('../../controller/product/product');
-const { update } = require('../../model/users');
 
 router.route('/')
 .get(getProduct)
